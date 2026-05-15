414fad2563a708b16f7c339010c34ab7e4740890
mavishak31 <mav.ishak31@gmail.com>
2026-05-15 18:46:27 +0700

    Complete Assignment 7 - Typescript - To-Do-App

--- dist/types.js
+++ dist/types.js
@@ -1,6 +1,4 @@
 "use strict";
 // TODO: Definisikan tipe data untuk To-Do item di sini
 // Hint: To-Do sebaiknya memiliki id, text, dan status completed
-// TODO: Buat interface untuk To-Do item
-// TODO: Buat tipe untuk status To-Do (active/done)
-// TODO: Buat tipe untuk fungsi-fungsi yang akan digunakan
+Object.defineProperty(exports, "__esModule", { value: true });
